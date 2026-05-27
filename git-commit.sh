574287724b182f046c2a388c2e29cfebecc3deb3
Khwaja Mahad Haq <kmahadh@gmail.com>
2026-05-27 03:14:52 -0400

    style: purge editor backup files from repository tracking

---
+++ git-commit.sh
@@ -0,0 +1,8 @@
+# Rename your default working branch to main
+git branch -M main
+
+# Link your local folder directly to your online GitHub repository profile 
+git remote add origin https://github.com/KMAHADH/SysMon-Dashboard.git
+
+# Push your code up to the secure cloud stream!
+git push -u origin main
